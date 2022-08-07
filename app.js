74ca1ac264d6c7861c164634962085390305046b
quro4ka <quro4ka@gmail.com>
2022-08-07 20:05:11 +0300

    change app

--- app.js
+++ app.js
@@ -10,14 +10,32 @@ function playShot(e)  {
     let element = e.target
 
     if (element.classList.contains('character')) {
+        hit.stop()
         hit.play()
         counter++
         count.textContent = counter;
         element.classList.add('die')
+        if (counter === 5) {
+            setTimeout(replay, 1000)
+        }
     } else {
+        shot.stop()
         shot.play()
     }
 }
 
+function replay() {
+    let died = document.querySelectorAll('.character')
+    died.forEach(item => {
+        item.classList.remove('die')
+    })
+    counter = 0
+    count.textContent = counter
+}
+
+HTMLAudioElement.prototype.stop = function () {
+    this.pause()
+    this.currentTime = 0.0
+}
 
 
